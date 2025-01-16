0c7f4e5d425a4632c3cc6c3574b674abdef1a263
tunatuna1733 <bouningen13@gmail.com>
2025-01-16 14:23:55 +0900

    fixed and added tests

--- scenario/rbs/param9.rb
+++ scenario/rbs/param9.rb
@@ -8,9 +8,14 @@ class C
   def foo(n)
     n.to_s
   end
+
+  def bar(n)
+    foo(n)
+  end
 end
 
 ## assert: test.rb
 class C
   def foo: (const Integer) -> String
+  def bar: (untyped) -> String
 end
